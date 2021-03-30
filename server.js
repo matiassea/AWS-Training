2e99b9044179f6a39bd27b04a2b4858be539ff6e
Matias <matiassea@hotmail.com>
2021-03-30 19:27:59 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>res.send('Hello World with express'))
+
+app.listen(3000);
+console.log("Server on port 3000")
+
+
