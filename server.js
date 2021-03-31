8103c7b944903bb1d20fca84e3cb67587d878e23
Matias Vidal <matiassea@hotmail.com>
2021-03-31 17:50:22 -0300

    caca maraco

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res)=>res.send('Hello World with express'))
+app.get('/',(req,res)=>res.send('<h1>Caca maraco</h1>'))
 
 app.listen(3000);
 console.log("Server on port 3000")
